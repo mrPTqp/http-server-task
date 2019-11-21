8b0112e7193341eb3bc7ea868fa91a2fd23d7720
mrPTqp <mrPTqp@gmail.com>
2019-11-21 17:30:00 +0300

    wip

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -1,3 +1,8 @@
-public interface HTTPServer {
+import java.io.IOException;
 
+public interface HTTPServer {
+    void setPort(int port);
+    void setCoreNumberOfThread(int coreNumberOfThread);
+    void setMaxNumberOfThread(int maxNumberOfThread);
+    void startServer(int port, int coreNumberOfThread, int maxNumberOfThread) throws IOException;
 }
