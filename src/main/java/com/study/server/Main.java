323b10b494ec5c2d2231761f6f76f2c7bf73320a
mrPTqp <mrPTqp@gmail.com>
2019-12-04 22:59:52 +0300

    wip

--- src/main/java/com/study/server/Main.java
+++ src/main/java/com/study/server/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServerImpl server = new HttpServerImpl(8080, 2, 8, 60);
-        server.start();
         System.out.println("Server is started");
+        server.start();
     }
 }
