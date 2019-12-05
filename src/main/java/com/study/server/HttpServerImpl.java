77b5dd996e9fea36a6e7feb03a5d93a3a297f2bd
mrPTqp <mrPTqp@gmail.com>
2019-12-05 23:37:20 +0300

    wip

--- src/main/java/com/study/server/HttpServerImpl.java
+++ src/main/java/com/study/server/HttpServerImpl.java
@@ -3,25 +3,17 @@ package com.study.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class HttpServerImpl implements HttpServer {
     private int port;
-    private int coreNumberOfThreads;
-    private int maxNumberOfThreads;
-    private int keepAliveTime;
+    private int numberOfThreads;
     private ExecutorService executor;
 
-    public HttpServerImpl(int port, int coreNumberOfThreads, int maxNumberOfThreads, int keepAliveTime) {
+    public HttpServerImpl(int port, int numberOfThreads) {
         this.port = port;
-        this.coreNumberOfThreads = coreNumberOfThreads;
-        this.maxNumberOfThreads = maxNumberOfThreads;
-        this.keepAliveTime = keepAliveTime;
-        this.executor = new ThreadPoolExecutor(coreNumberOfThreads, maxNumberOfThreads,
-                keepAliveTime, TimeUnit.SECONDS, new SynchronousQueue<>());
+        this.numberOfThreads = numberOfThreads;
+        this.executor = Executors.newFixedThreadPool(8);
     }
 
     @Override
--- src/main/java/com/study/server/Main.java
+++ src/main/java/com/study/server/Main.java
@@ -1,11 +1,40 @@
 package com.study.server;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServerImpl server = new HttpServerImpl(8080, 2, 8, 60);
-        System.out.println("Server is started");
-        server.start();
+        Map<String, Integer> parametersOfHttpServer;
+        String sourceConfig = System.getenv().get("SitesAndConfigDirectory");
+        parametersOfHttpServer = readProperties(sourceConfig);
+
+        if(parametersOfHttpServer != null) {
+            Integer port = parametersOfHttpServer.get("port");
+            Integer numberOfThreads = parametersOfHttpServer.get("numberOfThreads");
+
+            HttpServerImpl server = new HttpServerImpl(port, numberOfThreads);
+            System.out.println("Server is started");
+            server.start();
+        }
+    }
+
+    private static HashMap<String, Integer> readProperties(String sourceConfig) throws IOException {
+        HashMap<String, Integer> parametersOfHttpServer = new HashMap<>();
+        Properties properties = new Properties();
+        File file = new File(sourceConfig, "server-config.properties");
+
+        if (file.exists()) {
+            properties.load(new FileReader(file));
+            Integer port = Integer.parseInt(properties.getProperty("port"));
+            parametersOfHttpServer.put("port", port);
+            Integer numberOfThreads = Integer.parseInt(properties.getProperty("numberOfThreads"));
+            parametersOfHttpServer.put("numberOfThreads", numberOfThreads);
+            return parametersOfHttpServer;
+        } else {
+            return null;
+        }
     }
 }
