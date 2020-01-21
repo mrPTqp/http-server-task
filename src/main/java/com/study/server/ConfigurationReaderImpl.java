d547474167551e072ec0e4a4b365c2de421de7d6
mrPTqp <mrPTqp@gmail.com>
2020-01-21 22:35:43 +0300

    Exclude getHostFromPath. Add a condition to add host extracted from a path in mappings

--- src/main/java/com/study/server/ConfigurationReaderImpl.java
+++ src/main/java/com/study/server/ConfigurationReaderImpl.java
@@ -4,16 +4,9 @@ import com.study.server.utils.ParsingPatterns;
 
 import java.io.File;
 import java.io.FileReader;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Properties;
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class ConfigurationReaderImpl implements ConfigurationReader {
-    private static final String MESSAGE = "The configuration directory " +
-            "does not contain valid site directories";
     private String sourceDir;
 
     public ConfigurationReaderImpl() {
@@ -48,25 +41,20 @@ public class ConfigurationReaderImpl implements ConfigurationReader {
         File[] dirs = new File(sourceDir).listFiles(File::isDirectory);
 
         for (File curPath : dirs) {
-            String host = getHostFromPath(ParsingPatterns.pathHostPattern, curPath);
-            mutableMappings.put(host, curPath.toString());
+            String host;
+            var hostMatcher = ParsingPatterns.pathHostPattern.matcher(curPath.toString());
+            if (hostMatcher.find()) {
+                host = hostMatcher.group("pathHost");
+                mutableMappings.put(host, curPath.toString());
+            } else {
+                System.out.println("No directory matching host pattern was found in the path " + curPath.toString());
+            }
         }
 
         if (mutableMappings.isEmpty()) {
-            throw new NoSuchElementException(MESSAGE);
+            throw new NoSuchElementException("The configuration directory does not contain valid site directories");
         }
 
         return Collections.unmodifiableMap(mutableMappings);
-    }
-
-    private String getHostFromPath(Pattern hostPattern, File curDir) {
-        try {
-            var hostMatcher = hostPattern.matcher(curDir.toString());
-            hostMatcher.find();
-
-            return hostMatcher.group("pathHost");
-        } catch (Exception e) {
-            throw new NoSuchElementException(MESSAGE);
-        }
     }
 }
